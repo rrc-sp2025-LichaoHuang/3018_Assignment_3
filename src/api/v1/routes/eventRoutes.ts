9d3c88a68e959514f1787259d794df06b3976b74
Lichao Huang <lhuang38@rrc.ca>
2026-02-22 17:44:41 -0600

    feat: test joi and middleware

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -5,12 +5,15 @@ import { HTTP_STATUS } from "../../../constants/httpStatus";
 
 const router = Router();
 
-router.post(
-  "/",
-  validate(createEventSchema),
-  (req, res) => {
+router.post("/", validate(createEventSchema), (req, res) => {
     res.status(HTTP_STATUS.CREATED).json(req.body);
   }
 );
+
+router.get("/", (req, res) => {
+  res.status(HTTP_STATUS.OK).json({
+    message: "Events endpoint working",
+  });
+});
 
 export default router;
