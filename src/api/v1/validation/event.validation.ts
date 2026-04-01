3006d302cba2e50f9ca5b3db46944542f6947cd1
Lichao Huang <lhuang38@rrc.ca>
2026-03-31 19:49:47 -0500

    feat:open api for event.validation

--- src/api/v1/validation/event.validation.ts
+++ src/api/v1/validation/event.validation.ts
@@ -1,5 +1,46 @@
 import Joi from "joi";
-
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Event:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         date:
+ *           type: string
+ *         capacity:
+ *           type: number
+ *         registrationCount:
+ *           type: number
+ *         status:
+ *           type: string
+ *         category:
+ *           type: string
+ *
+ *     CreateEventInput:
+ *       type: object
+ *       required:
+ *         - name
+ *         - date
+ *         - capacity
+ *       properties:
+ *         name:
+ *           type: string
+ *         date:
+ *           type: string
+ *         capacity:
+ *           type: number
+ *         registrationCount:
+ *           type: number
+ *         status:
+ *           type: string
+ *         category:
+ *           type: string
+ */
 export const createEventSchema = Joi.object({
   name: Joi.string()
     .min(3)
